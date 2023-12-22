027c2cd46debe60e15ca5afcd9aef4bd31da366d
akash-crownstack <akash@crownstack.com>
2023-12-22 10:56:19 +0530

    Added max width wrapper

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -23,6 +23,7 @@ import React, { use, useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Label } from "@/components/ui/label";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 const Register = () => {
   const formSchema = z.object({
@@ -65,108 +66,110 @@ const Register = () => {
 
   return (
     <>
-      <Card>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(createUser)}>
-            <CardHeader className="space-y-1">
-              <CardTitle className="text-2xl">Create an account</CardTitle>
-              <CardDescription>
-                Enter your email below to create your account
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="firstName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>First Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="First Name" {...field} />
-                      </FormControl>
-                      <FormMessage className="text-red-600" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="lastName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Last Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Last Name" {...field} />
-                      </FormControl>
-                      <FormMessage className="text-red-600" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="abc@gmail.com"
-                          {...field}
-                          type="email"
-                        />
-                      </FormControl>
-                      <FormMessage className="text-red-600" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Password"
-                          {...field}
-                          type="password"
-                        />
-                      </FormControl>
-                      <FormMessage className="text-red-600" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="orgName"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Organisation Name</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Organisation Name" {...field} />
-                      </FormControl>
-                      <FormMessage className="text-red-600" />
-                    </FormItem>
-                  )}
-                />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button className="w-full" type="submit">
-                Create account
-              </Button>
-            </CardFooter>
-          </form>
-        </Form>
-      </Card>
+      <MaxWidthWrapper>
+        <Card>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(createUser)}>
+              <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl">Create an account</CardTitle>
+                <CardDescription>
+                  Enter your email below to create your account
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="grid gap-4">
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="firstName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>First Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="First Name" {...field} />
+                        </FormControl>
+                        <FormMessage className="text-red-600" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="lastName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Last Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Last Name" {...field} />
+                        </FormControl>
+                        <FormMessage className="text-red-600" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="abc@gmail.com"
+                            {...field}
+                            type="email"
+                          />
+                        </FormControl>
+                        <FormMessage className="text-red-600" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Password"
+                            {...field}
+                            type="password"
+                          />
+                        </FormControl>
+                        <FormMessage className="text-red-600" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="orgName"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Organisation Name</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Organisation Name" {...field} />
+                        </FormControl>
+                        <FormMessage className="text-red-600" />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button className="w-full" type="submit">
+                  Create account
+                </Button>
+              </CardFooter>
+            </form>
+          </Form>
+        </Card>
+      </MaxWidthWrapper>
     </>
   );
 };
